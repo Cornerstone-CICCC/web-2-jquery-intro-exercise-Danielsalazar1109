134670534b23d1e92662039eb0930e43d8d25aa0
Daniel Salazar <danielsalazar1109@gmail.com>
2024-07-29 17:04:56 -0700

    exercise done

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 $(function() {
-  // YOUR CODE HERE
+  const toggle=$("#toggleButton");
+  const color=$("#colorButton");
+  const addclass=$("#addClassButton");
+  const removeclass=$("#removeClassButton");
+  const fadein=$("#fadeInButton");
+  const fadeout=$("#fadeOutButton");
+  const slideup=$("#slideUpButton");
+  const slidedown=$("#slideDownButton");
+
+  toggle.click(function(){
+    $("#myParagraph").toggle();
+  });
+  color.click(function(){
+    $("#colorDiv").css("background-color", "yellow");
+  });
+  addclass.click(function(){
+    $("#classDiv").addClass("newClass");
+  });
+  removeclass.click(function(){
+    $("#classDiv").removeClass("newClass");
+  });
+  fadein.click(function(){
+    $("#fadeDiv").fadeIn();
+  });
+  fadeout.click(function(){
+    $("#fadeDiv").fadeOut();
+  });
+  slideup.click(function(){
+    $("#slideDiv").slideUp();
+  });
+  slidedown.click(function(){
+    $("#slideDiv").slideDown();
+  });
+  
 })
